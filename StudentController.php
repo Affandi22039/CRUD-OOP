65761d1f50a73c43c1cb61bd366d97bcce378d3b
Affandi <muhammadaffandi.22039@mhs.unesa.ac.id>
2023-12-01 21:55:30 +0700

    Add files via upload

---
+++ StudentController.php
@@ -0,0 +1,28 @@
+<?php
+
+class StudentController
+{
+    public function __construct()
+    {
+        $db = new DatabaseConnection;
+        $this->conn = $db->conn;
+    }
+
+    public function create($inputData)
+    {
+        $fullname = $inputData['fullname'];
+        $email = $inputData['email'];
+        $phone = $inputData['phone'];
+        $course = $inputData['course'];
+
+        $studentQuery = "INSERT INTO students (fullname,email,phone,course) VALUES ('$fullname','$email','$phone','$course')";
+        $result = mysqli_query($this->conn, $studentQuery);
+        if($result){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+?>
